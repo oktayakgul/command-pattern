2d1d1c7da9079617589072ff2dad836ea00c1bf1
Oktay <oktayakgul@outlook.com>
2022-07-11 16:42:47 +0300

    init

---
+++ src/test/java/com/oa/pattern/command/CommandPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oa.pattern.command;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
